7f9277d2d6f8bd6ba2941a8b164e27a4ca899713
Jorge Calavia <1184336+jcalavia@users.noreply.github.com>
2024-07-22 21:44:32 +0000

    New: Sanity checks passing

--- plugins/modules/dummy.py
+++ plugins/modules/dummy.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python
 
+# Ansible Collection Setup
+# Copyright (C) 2024  calavia.org
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import (absolute_import, division, print_function)
-from ansible.module_utils.basic import AnsibleModule
-__metaclass__ = type
 
 DOCUMENTATION = r'''
 ---
@@ -56,6 +70,9 @@ message:
     returned: always
     sample: 'goodbye'
 '''
+
+from ansible.module_utils.basic import AnsibleModule
+__metaclass__ = type
 
 
 def run_module():
