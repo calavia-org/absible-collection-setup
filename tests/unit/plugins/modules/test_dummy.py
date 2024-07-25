b87025417db3e0dbaf7c13b02db46b670fe5d298
Jorge Calavia <1184336+jcalavia@users.noreply.github.com>
2024-07-26 01:40:42 +0200

    Feature/ansible test (#11)
    * New: Refactor structure

--- tests/unit/plugins/modules/test_dummy.py
+++ tests/unit/plugins/modules/test_dummy.py
@@ -1,30 +1,18 @@
-'''
-Doc
-'''
 from __future__ import (absolute_import, division, print_function)
 import unittest
 __metaclass__ = type
 
 
 class AddTester(unittest.TestCase):
-    '''
-    Doc
-    '''
     a = 10
     b = 23
 
     # this function will
     def test_add(self):
-        '''
-        Doc
-        '''
-        c_var = 33
-        assert self.a + self.b == c_var
+        c = 33
+        assert self.a + self.b == c
 
     # this function will
     def test_subtract(self):
-        '''
-        Doc
-        '''
-        c_var = -13
-        assert self.a - self.b == c_var
+        c = -13
+        assert self.a - self.b == c
